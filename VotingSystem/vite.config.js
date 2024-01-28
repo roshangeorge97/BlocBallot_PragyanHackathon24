4c63943ef53bcb8c88fedca7fe129072fb5b6889
Roshan George <roshangeorge2003@gmail.com>
2024-01-28 20:18:21 +0530

    third2 commit1

--- VotingSystem/vite.config.js
+++ VotingSystem/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
@@ -8,5 +7,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  plugins: [reactRefresh()],
 })
+
